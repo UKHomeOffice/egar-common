5d228c43fec3356f7c0c09f6cba57e12dd540aec
Keshava Grama <Keshava.Grama@civica.co.uk>
2017-10-06 14:36:17 +0100

    Added interface for unregistered users in new datamodel

--- src/main/java/uk/gov/digital/ho/egar/services/auth/IEgarRegisteredUser.java
+++ src/main/java/uk/gov/digital/ho/egar/services/auth/IEgarRegisteredUser.java
@@ -1,5 +1,7 @@
 package uk.gov.digital.ho.egar.services.auth;
 
+import uk.gov.digital.ho.egar.constants.ServicePathConstants;
+
 /**
  * Registered users may have a minimal profile as their details are not required in eGAR if they are not 
  * travelling/ are not in a GAR
@@ -7,6 +9,12 @@ package uk.gov.digital.ho.egar.services.auth;
  *
  */
 public interface IEgarRegisteredUser {
+	public static final String ROOT_SERVICE_NAME = "auth";
+	public static final String ROOT_PATH = 
+			ServicePathConstants.ROOT_PATH_SEPERATOR + 
+			ServicePathConstants.ROOT_SERVICE_API + 
+			ServicePathConstants.ROOT_PATH_SEPERATOR + 
+			ROOT_SERVICE_NAME;
 	public enum AuthenticationState{
 		AUTHENTICATION_FAILED,
 		ACCOUNT_LOCKED,
---
+++ src/main/java/uk/gov/digital/ho/egar/services/auth/IEgarUnregisteredUser.java
@@ -0,0 +1,17 @@
+package uk.gov.digital.ho.egar.services.auth;
+
+import uk.gov.digital.ho.egar.constants.ServicePathConstants;
+/**
+ * This is base interface to indicating that there is a service to register users of 
+ * eGAR. Add method signatures for stuff that must be supported on the controller 
+ * @author Keshava.Grama
+ *
+ */
+public interface IEgarUnregisteredUser {
+	public static final String ROOT_SERVICE_NAME = "register";
+	public static final String ROOT_PATH = 
+			ServicePathConstants.ROOT_PATH_SEPERATOR + 
+			ServicePathConstants.ROOT_SERVICE_API + 
+			ServicePathConstants.ROOT_PATH_SEPERATOR + 
+			ROOT_SERVICE_NAME;
+}
