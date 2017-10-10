dca224afa8d8a3e6a770947d0777fc08d443d6d1
Keshava Grama <Keshava.Grama@civica.co.uk>
2017-10-10 12:37:23 +0100

    Updating data model with information that was accidentally removed

---
+++ src/main/java/uk/gov/digital/ho/egar/constants/ServicePathConstants.java
@@ -0,0 +1,6 @@
+package uk.gov.digital.ho.egar.constants;
+
+public interface ServicePathConstants {
+	public static final String ROOT_PATH_SEPERATOR = "/";
+	public static final String ROOT_SERVICE_API = "api";
+}
--- src/main/java/uk/gov/digital/ho/egar/services/v1/auth/UnregisteredUser.java
+++ src/main/java/uk/gov/digital/ho/egar/services/v1/auth/UnregisteredUser.java
@@ -1,5 +1,7 @@
 package uk.gov.digital.ho.egar.services.v1.auth;
 
+import uk.gov.digital.ho.egar.constants.ServicePathConstants;
+
 /**
  * This is base interface to indicating that there is a service to register users of 
  * eGAR. Add method signatures for stuff that must be supported on the controller 
@@ -7,5 +9,10 @@ package uk.gov.digital.ho.egar.services.v1.auth;
  *
  */
 public interface UnregisteredUser {
-    //TODO - Placeholder
+	public static final String ROOT_SERVICE_NAME = "register";
+	public static final String ROOT_PATH = 
+			ServicePathConstants.ROOT_PATH_SEPERATOR + 
+			ServicePathConstants.ROOT_SERVICE_API + 
+			ServicePathConstants.ROOT_PATH_SEPERATOR + 
+			ROOT_SERVICE_NAME;
 }
