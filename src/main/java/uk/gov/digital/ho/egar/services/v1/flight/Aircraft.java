d6b3853126a0fcc7e4461caa968e93eab83c68a7
Keshava Grama <Keshava.Grama@civica.co.uk>
2017-10-13 08:32:34 +0100

    Removed path constants from data model interfaces

--- src/main/java/uk/gov/digital/ho/egar/services/v1/flight/Aircraft.java
+++ src/main/java/uk/gov/digital/ho/egar/services/v1/flight/Aircraft.java
@@ -2,7 +2,6 @@ package uk.gov.digital.ho.egar.services.v1.flight;
 
 import java.util.UUID;
 
-import uk.gov.digital.ho.egar.constants.ServicePathConstants;
 import uk.gov.digital.ho.egar.services.v1.people.Person;
 import uk.gov.digital.ho.egar.services.v1.referencedata.AircraftType;
 
@@ -12,14 +11,6 @@ import uk.gov.digital.ho.egar.services.v1.referencedata.AircraftType;
  *
  */
 public interface Aircraft {
-	public static final String ROOT_SERVICE_NAME = "aircraft";
-	public static final String ROOT_PATH = 
-			ServicePathConstants.ROOT_PATH_SEPERATOR + 
-			ServicePathConstants.ROOT_SERVICE_API + 
-			ServicePathConstants.ROOT_PATH_SEPERATOR + 
-			ROOT_SERVICE_NAME;
-    
-	
 	UUID getUuid();
 
     AircraftType getAircraftType();
--- src/main/java/uk/gov/digital/ho/egar/services/v1/flight/Flight.java
+++ src/main/java/uk/gov/digital/ho/egar/services/v1/flight/Flight.java
@@ -3,21 +3,12 @@ package uk.gov.digital.ho.egar.services.v1.flight;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-import uk.gov.digital.ho.egar.constants.ServicePathConstants;
-
 /**
  * Interface for any method signatures and constants that the are relevant to a flight
  * @author Keshava.Grama
  *
  */
 public interface Flight {
-	public static final String ROOT_SERVICE_NAME = "flight";
-	public static final String ROOT_PATH = 
-			ServicePathConstants.ROOT_PATH_SEPERATOR + 
-			ServicePathConstants.ROOT_SERVICE_API + 
-			ServicePathConstants.ROOT_PATH_SEPERATOR + 
-			ROOT_SERVICE_NAME;
-	
 	UUID getId();
 
 	LocalDateTime getDepartureDateTimeUTC();
