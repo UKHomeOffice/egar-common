31ae5616b4ac6796b74f52cf5a15b16f5b40315d
Keshava Grama <Keshava.Grama@civica.co.uk>
2017-10-13 09:44:00 +0100

    Added version one to service constants for visibility

--- src/main/java/uk/gov/digital/ho/egar/constants/ServicePathConstants.java
+++ src/main/java/uk/gov/digital/ho/egar/constants/ServicePathConstants.java
@@ -3,4 +3,5 @@ package uk.gov.digital.ho.egar.constants;
 public interface ServicePathConstants {
 	public static final String ROOT_PATH_SEPERATOR = "/";
 	public static final String ROOT_SERVICE_API = "api";
+	public static final String SERVICE_VERSION_ONE = "v1";
 }
