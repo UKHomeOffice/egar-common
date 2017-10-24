934b025c52e486611c466adf8dc79db0d03562e5
Keshava Grama <Keshava.Grama@civica.co.uk>
2017-10-24 12:23:06 +0100

    Removed link to persisted record, updated name fields

--- src/main/java/uk/gov/digital/ho/egar/services/v1/people/Person.java
+++ src/main/java/uk/gov/digital/ho/egar/services/v1/people/Person.java
@@ -3,20 +3,18 @@ package uk.gov.digital.ho.egar.services.v1.people;
 import java.time.LocalDate;
 import java.util.UUID;
 
-import uk.gov.digital.ho.egar.datamodel.PersistedRecord;
-
 /**
  * Interface for any method signatures and constants for all person records in eGAR
  * @author Keshava.Grama
  *
  */
-public interface Person extends PersistedRecord{
+public interface Person{
 
 	UUID getUuid();
 
-	String getGivenNames();
+	String getFirstNames();
 
-	String getSurname();
+	String getLastNames();
 
 	Gender getGender();
 
