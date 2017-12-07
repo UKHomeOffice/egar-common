425dda5301656f412ba17fe02ce552826aee7ad2
Unknown <hamza.khaleel@civica.co.uk>
2017-12-07 11:05:55 +0000

    Added implemention on getting user uuid

---
+++ src/main/java/uk/gov/digital/ho/egar/shared/auth/api/token/AuthValues.java
@@ -0,0 +1,50 @@
+package uk.gov.digital.ho.egar.shared.auth.api.token;
+
+import java.util.UUID;
+
+import org.springframework.http.HttpHeaders;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class AuthValues {
+
+	public static final String USERID_HEADER = "x-auth-subject";
+	public static final String AUTH_HEADER = "Authorization";
+	
+	private final String authHeader;
+	private final UUID userUuid;
+	
+	public HttpHeaders createHttpHeaders() {
+		
+		HttpHeaders headers = new HttpHeaders();
+		
+		addUserUuid(headers);
+		addAuthHeader(headers);
+		
+		return headers ;
+	}
+
+	private void addAuthHeader(HttpHeaders headers) {
+		
+		if ( this.getAuthHeader() != null )
+		{
+			headers.add(AUTH_HEADER, this.getAuthHeader() );
+		}
+		
+	}
+
+	private void addUserUuid(HttpHeaders headers) {
+
+		if ( this.getUserUuid() != null )
+		{
+			headers.add(USERID_HEADER, this.getUserUuid().toString() );
+		}
+
+		
+	}
+	
+	
+}
