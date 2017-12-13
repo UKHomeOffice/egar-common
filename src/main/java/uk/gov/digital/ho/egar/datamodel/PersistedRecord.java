eba9ed674c32ab28d94584ee383c847d8f25ee49
Bruce Mundin <bruce.mundin@ipl.com>
2017-12-13 11:04:16 +0000

    EGAR-998 | deprecated two classes.

--- src/main/java/uk/gov/digital/ho/egar/datamodel/PersistedRecord.java
+++ src/main/java/uk/gov/digital/ho/egar/datamodel/PersistedRecord.java
@@ -9,7 +9,7 @@ import java.util.UUID;
 /**
  * Parent interface for all persisted records in the database. It will be used to automate the cleanup of records
  * @author Keshava.Grama
- *
+ * @deprecated Not needed at this time as it is not part of requirements. Remove in a future sprint.
  */
 public interface PersistedRecord extends Serializable{
 
--- src/main/java/uk/gov/digital/ho/egar/datamodel/PurgePeriod.java
+++ src/main/java/uk/gov/digital/ho/egar/datamodel/PurgePeriod.java
@@ -1,5 +1,8 @@
 package uk.gov.digital.ho.egar.datamodel;
 
+/**
+ * @deprecated Not needed at this time as it is not part of requirements. Remove in a future sprint.
+ */
 public enum PurgePeriod {
     THREE_MONTHS,
     SIX_MONTHS,
