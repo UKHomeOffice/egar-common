6ab026d6b4ec353a2406faf4a175fdf3e57172cf
Bruce Mundin <bruce.mundin@ipl.com>
2017-12-15 18:01:04 +0000

    EGAR-899 | ZonedDateSerializer added.

---
+++ src/main/java/uk/gov/digital/ho/egar/shared/util/exceptions/ZonedDateSerializer.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package uk.gov.digital.ho.egar.shared.util.exceptions;
+
+import java.io.IOException;
+import java.time.ZonedDateTime;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+
+public class ZonedDateSerializer extends JsonSerializer<ZonedDateTime> {
+
+	@Override
+	public void serialize(ZonedDateTime value, JsonGenerator gen, SerializerProvider serializers)
+			throws IOException, JsonProcessingException {
+		gen.writeString(value.toString());
+	}
+
+}
