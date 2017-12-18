1299f2a83025e1576529ae1bf7b007b9e752d71a
Bruce Mundin <bruce.mundin@ipl.com>
2017-12-18 11:07:23 +0000

    [4.3.0] -- EGAR-899 | Commented NoCallStackException

--- src/main/java/uk/gov/digital/ho/egar/shared/util/exceptions/NoCallStackException.java
+++ src/main/java/uk/gov/digital/ho/egar/shared/util/exceptions/NoCallStackException.java
@@ -2,12 +2,19 @@ package uk.gov.digital.ho.egar.shared.util.exceptions;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 
+/**
+ * This is a base exception class to prevent the JVM from scanning the Java call stack unnecessarily when throwing an exception.
+ * This is a performance improvement as determining the call stack takes 100x times longer to construct the object.
+ * This object also may be used to create a JSON response for system errors so it suppresses empty methods.
+ * Such as {@link #getCause()} and {@link #getStackTrace()}.
+ */
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
 public abstract class NoCallStackException extends Exception {
 
 	private static final long serialVersionUID = 1L;
 
 	public NoCallStackException(String message, Throwable cause) {
+		// This is the call that turns off the stack trace "finding".
 		super(message, cause, false, false);
 	}
 
